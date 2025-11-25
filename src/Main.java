dbccb822e93ef9c3344c7319505c1070b22b2e86
Anna Markovsky <156268866+anna-markovsky@users.noreply.github.com>
2025-11-24 20:33:01 -0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import cmpt213.A4.model.Game;
+import cmpt213.A4.userinterface.StatsTracker;
+import cmpt213.A4.model.WeaponManager;
+import cmpt213.A4.userinterface.TextUI;
+
+public class Main {//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+    // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Game game = new Game();
+        TextUI ui = new TextUI(game);
+        WeaponManager weaponManager = new WeaponManager(game);
+        StatsTracker statsTracker = new StatsTracker(game);
+        ui.playGame();
+    }
+}
