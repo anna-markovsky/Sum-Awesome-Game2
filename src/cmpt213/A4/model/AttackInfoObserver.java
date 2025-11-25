7ed07da6a82370a2079e45f516925573b719121a
Anna Markovsky <156268866+anna-markovsky@users.noreply.github.com>
2025-11-24 20:32:23 -0800

    Delete src directory

--- src/cmpt213/A4/model/AttackInfoObserver.java
+++
@@ -1,6 +0,0 @@
-package cmpt213.A4.model;
-
-public interface AttackInfoObserver {
-    void getAttackInformation(String weaponName, double[] damageRecieved);
-    void getFillAttackInformation(int damageRecieved,Opponent selectedOpponent, int opponentIndex);
-}
