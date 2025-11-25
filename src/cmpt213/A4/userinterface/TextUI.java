712685782a2081c9d3be32e675b4569530a2cb42
maxwammoschool <maxwang021105@gmail.com>
2025-11-24 22:10:04 -0800

    fixed ring

--- src/cmpt213/A4/userinterface/TextUI.java
+++ src/cmpt213/A4/userinterface/TextUI.java
@@ -223,9 +223,13 @@ public class TextUI {
     }
 
     private void selectRingToEquip(String[] args){
-        for(int i = 1; i < 4; i++){
-            game.unequipRing(i - 1);
-            game.equipRing(Integer.parseInt(args[i]));
+        if(args.length < 4){
+            System.out.println("Please provide 3 ring numbers");
+        }else{
+            for(int i = 1; i < 4; i++){
+                game.unequipRing(0);
+                game.equipRing(Integer.parseInt(args[i]));
+            }
         }
     }
 
