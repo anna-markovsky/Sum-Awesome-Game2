cf9e9f03d776b7487868d74d4a96ba122f53b6ac
Anna Markovsky <156268866+anna-markovsky@users.noreply.github.com>
2025-11-24 18:36:43 -0800

    Delete src directory

--- src/cmpt213/A4/model/UserInterfaceObserver.java
+++
@@ -1,9 +0,0 @@
-package cmpt213.A4.model;
-
-/**
- * Interface for observers to implement to be able to observe
- * and react when UI makes request to display stats
- */
-public interface UserInterfaceObserver {
-    void printRequestFromUI();
-}
